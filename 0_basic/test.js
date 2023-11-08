18dada8b0a1aae303a2b80545df13317bde4eed4
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2023-11-08 15:00:55 +0000

    container config done

--- 0_basic/test.js
+++ 0_basic/test.js
@@ -1,1 +1,8 @@
-console.log("Ajmeri")
+const accountId=12342
+
+let accountEmail="afsgs@gmail.com"
+var accountpassword="hfei99"
+accountcity="jaipur"
+ 
+console.log(accountId)
+console.table([accountId, accountpassword ,accountEmail,accountcity] )
