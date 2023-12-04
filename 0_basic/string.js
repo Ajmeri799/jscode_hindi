44695e08b90a177387c78175d0142ed2282faa29
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2023-12-04 20:16:06 +0530

    Create string.js

---
+++ 0_basic/string.js
@@ -0,0 +1,26 @@
+const name="ajmeri"
+const name2=" sania"
+console.log(name+name2 + " value")
+console.log(`${name}${name2}`)
+const gamename=new String("ludogame")
+console.log(gamename)
+console.log(gamename[0])
+console.log(gamename.__proto__)
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2))
+console.log(gamename.indexOf('d'))
+// sub string
+console.log(gamename.substring(0,4))
+console.log(gamename.slice(-8,4))
+ const newString ="   Ajmeri  "
+ console.log(newString)
+ console.log(newString.trim())
+ const url="https://www.programiz.com%20javascript/online-compiler/"
+ console.log(url.replace('%20','/'))
+
+
+ console.log(url.includes("sunder"))
+  
+ const stringA="ajmeri-guniya-laveena"
+  console.log(stringA.split("-"))
