3e01b29c4d760713b6dc8dcf5d4803e570b38aae
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2023-12-09 14:36:43 +0000

    container config done

---
+++ 06_controlflow/one.js
@@ -0,0 +1,24 @@
+const name="ajmeri"
+if(name){
+    console.log("welcome")
+}
+
+// const name = " "
+// if(name){
+//     console.log("welcome")
+// }
+
+if(2==='2'){
+    console.log("two")
+}
+else{
+    console.log("error")
+}
+
+// === use cheack with data type
+if(2==0){
+    console.log("two")
+}
+else{
+    console.log("error")
+}
---
+++ 06_controlflow/switch.js
@@ -0,0 +1,17 @@
+const roll_no=3
+switch(roll_no)
+{
+    case 1:
+    console.log("ajmeri")
+    break;
+    case 2:
+    console.log("bittu")
+    break;
+    case 3:
+    console.log("sania")
+    break;
+    default:
+    console.log("kaif")
+    break;
+    
+}
