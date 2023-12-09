f266d7ed9079cbea758a61d14533ed2ce0c589c0
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2023-12-09 15:08:43 +0000

    container  config done

---
+++ 06_controlflow/truthy.js
@@ -0,0 +1,31 @@
+// falsy value 
+
+// false ,0,-0,BigInt 0n,"",null,undefined,NaN
+
+// truthy value 
+
+// true ,'0','false',"",[],{},function(){}
+
+
+// const num=[]
+// if(num){
+//   console.log("Array is empty")  
+// }
+
+// const num=[]
+// if(num.length===0){
+//   console.log("Array is empty")  
+// }
+
+//nullish coalescing Operator (??):null undefined
+
+let val1;
+val1=5 ?? 10
+//val1=null?? 10
+//val1=undefined ?? 10
+//val1=null?? 10??20
+
+console.log(val1)
+
+
+
