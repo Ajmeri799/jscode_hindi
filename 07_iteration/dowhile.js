c283d59339a4dc264d64b08ae9bd817215f6eef9
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2023-12-09 16:59:20 +0000

    container config done

---
+++ 07_iteration/dowhile.js
@@ -0,0 +1,35 @@
+
+// let i=1
+// let j=1
+// while(i<=10){
+// //console.log(i)
+// i++
+
+// }
+
+// const myarr=["1","2","3","4"]
+// console.log(myarr.length)
+// let arr=0
+// while(arr<myarr.length){
+//     console.log(myarr[arr])
+//     arr=arr+1
+// }
+
+// let z=1
+// do{
+//     console.log(z)
+//     z++
+// }while(z<=10);
+let z=1
+do{
+    console.log(z)
+    z++
+}while(z=0)
+
+
+
+
+
+
+
+
