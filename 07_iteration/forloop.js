6702cec748d80b69e451a5b0713b0cbbbe851f90
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2023-12-09 16:58:09 +0000

    container config done

---
+++ 07_iteration/forloop.js
@@ -0,0 +1,31 @@
+
+// for(let i=1; i<=10;i++){
+//   // console.log(i)
+//     for(let j=1;j<=10;j++)
+//     console.log(i*j)
+    
+// }
+for(let j=0;j<=10;j++){
+    
+    if(j==5){
+        console.log("5 is good num")
+    }
+    console.log(j)
+}
+
+for(let j=0;j<=10;j++){
+    
+    if(j==5){
+        console.log("5 is good num")
+        break;
+    }
+    console.log(j)
+}
+for(let j=0;j<=10;j++){
+    
+    if(j==5){
+        console.log("5 is good num")
+        continue;
+    }
+    console.log(j)
+}
