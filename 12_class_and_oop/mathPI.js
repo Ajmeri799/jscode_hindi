5845f23fae3d8e12d23faec95294a789c293cfa7
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2023-12-28 18:12:03 +0000

    container config done

--- 12_class_and_oop/mathPI.js
+++ 12_class_and_oop/mathPI.js
@@ -1,1 +1,12 @@
 console.log(Math.PI);
+//  const desc=Object.getOwnPropertyDescriptor(Math,"PI")
+// console.log(desc)
+// console.log(Math.PI);
+
+let chai={
+    name:"bmc chai",
+    rate:250,
+    order:true
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"))
