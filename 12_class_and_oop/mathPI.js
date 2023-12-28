d5eca9690ad62cbaf0c585d84e8bb3aaef8b6587
Ajmeri-Guniya-14 <149288719+AjmeriGuniya@users.noreply.github.com>
2023-12-28 19:22:03 +0000

    container config done

--- 12_class_and_oop/mathPI.js
+++ 12_class_and_oop/mathPI.js
@@ -1,4 +1,4 @@
-console.log(Math.PI);
+// console.log(Math.PI);
 //  const desc=Object.getOwnPropertyDescriptor(Math,"PI")
 // console.log(desc)
 // console.log(Math.PI);
@@ -6,7 +6,30 @@ console.log(Math.PI);
 let chai={
     name:"bmc chai",
     rate:250,
-    order:true
+    at_available:true,
+    order:function(){
+console.log("order");
+    } 
 }
 
-console.log(Object.getOwnPropertyDescriptor(chai,"name"))
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+Object.defineProperty(chai,'name',{
+   
+    writable: false,
+    // enumerable: false,
+    enumerable: true,
+    configurable: false
+
+})
+
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+
+
+for (let [key,value] of Object.entries(chai)) {
+    if(typeof value !== "function"){
+    console.log(`${key}:${value}`)
+    }
+}
